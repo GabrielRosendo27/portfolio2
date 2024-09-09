316d1790fdcc7f8cf5b3a8632254e4d484c3375d
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2024-09-09 16:00:16 -0300

    menu

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const MenuStyle = styled.div`
+  width: 100vw;
+  height: 80px;
+  position: fixed;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 250px;
+`;
+
+const MenuUl = styled.ul`
+  display: flex;
+  gap: 15px;
+`;
+const MenuLi = styled.li`
+  font-size: 22px;
+`;
+const MenuA = styled.a`
+  padding: 6px 8px;
+
+  border-radius: 4px;
+`;
+
+const Menu = () => {
+  return (
+    <MenuStyle>
+      <MenuUl>
+        <MenuLi>Gabriel Rosendo</MenuLi>
+      </MenuUl>
+      <MenuUl>
+        <MenuLi>
+          <MenuA>Ínicio</MenuA>
+        </MenuLi>
+        <MenuLi>
+          <MenuA>Sobre</MenuA>
+        </MenuLi>
+        <MenuLi>
+          <MenuA>Portfólio</MenuA>
+        </MenuLi>
+        <MenuLi>
+          <MenuA>Contato</MenuA>
+        </MenuLi>
+      </MenuUl>
+    </MenuStyle>
+  );
+};
+
+export default Menu;
