b20193a660a8d5e1969749569db1ed1bd312a7d4
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2024-09-12 06:53:39 -0300

    data-code

---
+++ src/Data/Data.js
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    id: "1",
+    image: "./listafilmesimg.PNG",
+    title: "Lista Filmes App",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?",
+    tec: "React, Firebase, API",
+    git: "Link Github",
+  },
+  {
+    id: "2",
+    image: "./imgBotDisc.PNG",
+    title: "Discord Bot",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?",
+    tec: "Python, SQLite",
+    git: "Link Github",
+  },
+  {
+    id: "3",
+    image: "./exame.PNG",
+    title: "Exame",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?",
+    tec: "Java",
+    git: "Link Github",
+  },
+  {
+    id: "4",
+    image: "./telaLogin.PNG",
+    title: "Tela de login",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?",
+    tec: "Java, SQL",
+    git: "Link Github",
+  },
+  {
+    id: "5",
+    image: "./jogodavelha.png",
+    title: "Jogo da Velha",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?",
+    tec: "HTML, CSS, JavaScript",
+    git: "Link Github",
+  },
+  {
+    id: "6",
+    image: "./bikcraft.png",
+    title: "Bikcraft",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi reprehenderit esse quam quasi corrupti aut quis alias amet iure consectetur nesciunt, nobis reiciendis facere mollitia ipsam voluptatibus. Vero, est nihil?",
+    tec: "HTML, CSS",
+    git: "Link Github",
+  },
+];
